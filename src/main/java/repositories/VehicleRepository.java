6f25ae1600e06bff96971170a09ab265f0c5a7e3
Marcin Rycek <martin1234543@gmail.com>
2025-03-26 17:10:45 +0100

    przyklad

---
+++ src/main/java/repositories/VehicleRepository.java
@@ -0,0 +1,12 @@
+package repositories;
+
+import model.Vehicle;
+
+import java.util.List;
+import java.util.Optional;
+public interface VehicleRepository {
+    List<Vehicle> findAll();
+    Optional<Vehicle> findById(String id);
+    Vehicle save(Vehicle vehicle);
+    void deleteById(String id);
+}
---
+++ src/main/java/repositories/impl/VehicleJsonRepository.java
@@ -0,0 +1,52 @@
+package repositories.impl;
+
+import com.google.gson.reflect.TypeToken;
+import model.Vehicle;
+import repositories.VehicleRepository;
+import utils.JsonFileStorage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class VehicleJsonRepository
+        implements VehicleRepository {
+    private final JsonFileStorage<Vehicle> storage =
+            new JsonFileStorage<>("vehicles.json", new
+                    TypeToken<List<Vehicle>>() {
+                    }.getType());
+    private final List<Vehicle> vehicles;
+    public VehicleJsonRepository() {
+        this.vehicles = new
+                ArrayList<>(storage.load());
+    }
+
+    @Override
+    public Vehicle save(Vehicle vehicle) {
+        if (vehicle.getId() == null ||
+                vehicle.getId().isBlank()) {
+            vehicle.setId(UUID.randomUUID().toString());
+        }
+        else {
+            deleteById(vehicle.getId());
+        }
+        vehicles.add(vehicle);
+        storage.save(vehicles);
+        return vehicle;
+    }
+    @Override
+    public List<Vehicle> findAll() {
+        return new ArrayList<>(vehicles);
+    }
+    @Override
+    public Optional<Vehicle> findById(String id) {
+        return vehicles.stream().filter(v ->
+                v.getId().equals(id)).findFirst();
+    }
+    @Override
+    public void deleteById(String id) {
+        vehicles.removeIf(v -> v.getId().equals(id));
+        storage.save(vehicles);
+    }
+}
